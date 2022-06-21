f98d2a5c60ad9087a147118c32316a8b0a39c0b7
OSKC <oskc@nnit.com>
2022-06-21 15:19:03 +0200

    Failed commit

--- src/ElementPicker.tsx
+++ src/ElementPicker.tsx
@@ -6,6 +6,9 @@ import {Property} from "csstype";
 
 function ElementPicker(props: ElementPickerProps) {
   const ref_container = useRef<HTMLDivElement>(null);
+
+  const [flag_ctrl, setFlagCtrl] = useState(false);
+  const [flag_shift, setFlagShift] = useState(false);
   const [selection_point, setSelectionPoint] = useState<Point>();
   const [selection_rect, setSelectionRect] = useState<Rect>();
 
@@ -14,71 +17,167 @@ function ElementPicker(props: ElementPickerProps) {
   const [scroll_speed_y, setScrollSpeedY] = useState<number>(0);
 
   const ref_variables = useRef<Reference>();
-  ref_variables.current = {selection_rect, selection_point, scroll_timer, scroll_speed_x, scroll_speed_y};
+  ref_variables.current = {selection_rect, selection_point, scroll_timer, scroll_speed_x, scroll_speed_y, flag_ctrl, flag_shift};
 
   const onMouseDown = useCallback((event: React.MouseEvent) => {
     if (event.button !== 0) return;
     if (!ref_container.current) throw new Error("Reference element does not exist. No element to select inside.");
+
+    // Shift and ctrl keys are locked on mouse down for the rest of the operation.
+    if (event.shiftKey) setFlagShift(true);
+    if (event.ctrlKey) setFlagCtrl(true);
+
     setSelectionPoint(new Point(event.pageX, event.pageY).confineToElement(ref_container.current));
     setScrollTimer(window.setInterval(() => {
-      if (!ref_container.current || !ref_variables.current) return;
+      const {current: container} = ref_container;
+      if (!container || !ref_variables.current) return;
 
       const {selection_rect, scroll_speed_x, scroll_speed_y} = ref_variables.current;
-      if (!selection_rect || scroll_speed_x === 0 && scroll_speed_y === 0) return;
+      if (scroll_speed_x === 0 && scroll_speed_y === 0) return;
 
-      const scroll = scrollElementBy(ref_container.current, scroll_speed_x, scroll_speed_y);
+      const scroll = scrollElementBy(container, scroll_speed_x, scroll_speed_y);
       if (!scroll.y) setScrollSpeedY(0);
       if (!scroll.x) setScrollSpeedX(0);
 
-      const rect = extendRectWithin(selection_rect, ref_container.current, scroll_speed_x, scroll_speed_y);
-      setSelectionRect(rect);
-      props.onHover?.(getElementListInRect(ref_container.current, rect, props.threshold), rect);
+      if (selection_rect) {
+        const rect = extendRectWithin(selection_rect, container, scroll.x, scroll.y);
+        setSelectionRect(rect);
+        props.onHover?.(getElementListInRect(container, rect, props.select_threshold), rect);
+      }
     }, 20));
+
     window.addEventListener("mouseup", onMouseUp);
     window.addEventListener("mousemove", onMouseMove);
   }, []);
 
   const onMouseMove = useCallback((event: MouseEvent) => {
-    if (!ref_variables.current?.selection_point) throw new Error("Selection point has not been created before window.onMouseMove event was triggered.");
-    if (!ref_container.current) throw new Error("Reference element does not exist. No element to select inside.");
+    // Initialization to ensure variables exist in current context
+    const {current: container} = ref_container;
+    if (!container) throw new Error("Reference element does not exist. No element to select inside.");
 
-    const cursor = new Point(event.pageX, event.pageY);
-    const point = new Point(event.pageX, event.pageY).confineToElement(ref_container.current);
-    if (!ref_variables.current.selection_rect && point.distanceToPoint(ref_variables.current.selection_point) < 8) return;
+    const selection_point = ref_variables.current?.selection_point;
+    if (!selection_point) throw new Error("Selection point has not been created before window.onMouseMove event was triggered.");
 
-    const rect = Rect.fromPoints(point, ref_variables.current.selection_point);
-    props.onHover?.(getElementListInRect(ref_container.current, rect, props.threshold), rect);
-    setSelectionRect(rect);
-
-    const {x, y} = getScrollSpeed(ref_container.current.getBoundingClientRect(), cursor, props.scroll_coefficient);
-    console.log(x, y);
+    // Get scroll speed and update variables. Selection rect does not need to exist to start scrolling.
+    const {x, y} = getScrollSpeed(container.getBoundingClientRect(), new Point(event.pageX, event.pageY), props.scroll_coefficient);
     setScrollSpeedX(x);
     setScrollSpeedY(y);
+
+    // Check if selection rect exists and if not, ensure there's a certain a certain distance 
+    // from origin point to current point, before creating the selection rect.
+    const point = new Point(event.pageX, event.pageY).confineToElement(container);
+    if (!ref_variables.current?.selection_rect && point.distanceToPoint(selection_point) < 8) return;
+
+    const rect = Rect.fromPoints(point, selection_point);
+    setSelectionRect(rect);
+
+    const selection = getElementListInRect(container, rect, props.select_threshold);
+    if (ref_variables.current?.flag_ctrl) {
+      // If ctrl is held, we will expand and negate truthy selections
+      selection.forEach((value, index) => {selection[index] = selection[index] || !props.selection?.[index]});
+    }
+    else if (ref_variables.current?.flag_shift) {
+      // If shift is held, we will expand selection
+      selection.forEach((value, index) => {selection[index] = selection[index] || !!props.selection?.[index]});
+    }
+    props.onHover?.(selection, rect);
   }, []);
 
   const onMouseUp = useCallback((event: MouseEvent) => {
-    window.clearInterval(ref_variables.current?.scroll_timer);
+    // Clear all handlers
     window.removeEventListener("mouseup", onMouseUp);
     window.removeEventListener("mousemove", onMouseMove);
-    if (!ref_container.current) throw new Error("Reference element does not exist. No element to select inside.");
-    if (!ref_variables.current?.selection_point) throw new Error("Selection point has not been created before window.onMouseUp event was triggered.");
-
-    const rect = ref_variables.current?.selection_rect ?? Rect.fromPoints(Point.fromEventPage(event).confineToElement(ref_container.current), ref_variables.current.selection_point);
-    const element_list = getElementListInRect(ref_container.current, rect, props.threshold);
-
-    if (!ref_variables.current?.selection_rect) props.onClick?.(element_list.findIndex(value => value), rect, event);
-    props.onHover?.(getEmptyList(ref_container.current), rect);
-    props.onSelect?.(element_list, rect, event);
-    setSelectionRect(undefined);
-    setSelectionPoint(undefined);
+    window.clearInterval(ref_variables.current?.scroll_timer);
     setScrollTimer(undefined);
     setScrollSpeedX(0);
     setScrollSpeedY(0);
+    setFlagCtrl(false);
+    setFlagShift(false);
+
+    // Initialization to ensure variables exist in current context
+    const {current: container} = ref_container;
+    if (!container) throw new Error("Reference element does not exist. No element to select inside.");
+
+    const selection_point = ref_variables.current?.selection_point;
+    if (!selection_point) throw new Error("Selection point has not been created before window.onMouseUp event was triggered.");
+    setSelectionPoint(undefined);
+
+    // Calculate final selection rect, clear current selection rect and hover list.
+    const selection_rect = ref_variables.current?.selection_rect ?? Rect.fromPoints(selection_point, Point.fromEventPage(event).confineToElement(container));
+    props.onHover?.();
+    setSelectionRect(undefined);
+
+    if (ref_variables.current?.selection_rect) {
+      // We have a drag selection
+      const selection = getElementListInRect(container, selection_rect, props.select_threshold);
+      if (ref_variables.current?.flag_ctrl) {
+        // If ctrl is held, we will expand and negate truthy selections
+        selection.forEach((value, index) => {selection[index] = selection[index] || !props.selection?.[index]});
+      }
+      else if (ref_variables.current?.flag_shift) {
+        // If shift is held, we will expand selection
+        selection.forEach((value, index) => {selection[index] = selection[index] || !!props.selection?.[index]});
+      }
+      props.onSelect?.(selection, selection_rect, event);
+    }
+    else {
+      // Performig a normal click
+      const selection = getEmptyList(container);
+      if (ref_variables.current?.flag_ctrl) {
+        getElementListInRect(container, selection_rect, props.select_threshold).forEach((value, index) => {selection[index] = !(props.selection?.[index] || !value) })
+      }
+      if (ref_variables.current?.flag_shift) {
+        for (let i = 0; i < container.children.length; i++) {
+          const child = container.children.item(i);
+          if (child && props.selection?.[i]) selection_rect.union(child.getBoundingClientRect());
+        }
+      }
+
+
+      if (ref_variables.current?.flag_shift) {
+        // Shift and ctrl is held - Expand selection to include current selection 
+        // and select all elements in the greater selection
+        for (let i = 0; i < container.children.length; i++) {
+          const child = container.children.item(i);
+          if (child) selection_rect.union(child.getBoundingClientRect());
+        }
+        const selection = getElementListInRect(container, selection_rect, props.select_threshold);
+        props.onSelect?.(selection, selection_rect, event);
+      }
+      if (ref_variables.current?.flag_shift) {
+        // Shift is held - Find the focus and select everything 
+        const focus = props.focus ?? 0;
+        const element = container.children.item(focus);
+        if (!element) throw new Error("Could not find focused element.");
+        selection_rect.union(element.getBoundingClientRect());
+        const selection = getElementListInRect(container, selection_rect, props.select_threshold);
+        props.onSelect?.(selection, selection_rect, event);
+      }
+      if (props.selection && flag_ctrl) {
+        const selection = getElementListInRect(container, selection_rect, props.select_threshold).reduce(
+          (result, value, index) => {
+            result[index] = value || result[index];
+            return result;
+          },
+          [] as boolean[]
+        );
+        props.onSelect?.(selection, selection_rect, event);
+      }
+
+      if (props.selection && flag_shift) {
+        // If we already have a selection, 
+
+        const selection = getElementListInRect(container, selection_rect, props.select_threshold);
+      }
+    }
+
+    props.onSelect?.(element_list, selection_rect, event);
+    if (!ref_variables.current?.selection_rect) props.onClick?.(element_list.findIndex(value => value), selection_rect, event);
   }, []);
 
-  const position_rect = {"--display": "none"} as ElementPickerRectCSSProperties & CSSProperties;
+  const position_rect = {} as ElementPickerRectCSSProperties & CSSProperties;
   if (selection_rect) {
-    delete position_rect["--display"];
+    position_rect["--display"] = "block";
     position_rect["--top"] = `${selection_rect.y}px`;
     position_rect["--left"] = `${selection_rect.x}px`;
     position_rect["--width"] = `${selection_rect.width}px`;
@@ -94,7 +193,7 @@ function ElementPicker(props: ElementPickerProps) {
   );
 }
 
-function getScrollSpeed(rect: DOMRect, cursor: Point, coefficient: number = 15) {
+function getScrollSpeed(rect: DOMRect, cursor: Point, coefficient: number = 15): {x: number, y: number} {
   const speed = {x: 0, y: 0};
   const scroll_coefficient = 1 / coefficient * 100;
 
@@ -104,9 +203,6 @@ function getScrollSpeed(rect: DOMRect, cursor: Point, coefficient: number = 15) 
   else if (rect.y + rect.height < cursor.y) {
     speed.y = (cursor.y - rect.y - rect.height) / scroll_coefficient;
   }
-  else {
-    speed.y = 0;
-  }
 
   if (rect.x > cursor.x) {
     speed.x = (cursor.x - rect.x - rect.width) / scroll_coefficient;
@@ -114,14 +210,11 @@ function getScrollSpeed(rect: DOMRect, cursor: Point, coefficient: number = 15) 
   else if (rect.x + rect.width < cursor.x) {
     speed.x = (cursor.x - rect.x) / scroll_coefficient;
   }
-  else {
-    speed.x = 0;
-  }
 
   return speed;
 }
 
-function extendRectWithin(rect: Rect, element: HTMLElement, x: number, y: number) {
+function extendRectWithin<T extends Rect>(rect: T, element: HTMLElement, x: number, y: number): T {
   if (x < 0) {
     const delta = rect.x - Math.max(0, rect.x + x);
     rect.x -= delta;
@@ -143,7 +236,7 @@ function extendRectWithin(rect: Rect, element: HTMLElement, x: number, y: number
   return rect;
 }
 
-function scrollElementBy(element: HTMLElement, left: number, top: number) {
+function scrollElementBy(element: HTMLElement, left: number, top: number): {x: number, y: number} {
   let {scrollTop: x, scrollLeft: y} = element;
   element.scrollBy({behavior: "auto", left, top});
   x = element.scrollLeft - x;
@@ -151,35 +244,31 @@ function scrollElementBy(element: HTMLElement, left: number, top: number) {
   return {x, y};
 }
 
-function getElementListInRect(element: HTMLElement, rect: Rect, threshold: number = 0.2) {
+function getElementListInRect(element: HTMLElement, rect: Rect, threshold: number = 0.2): boolean[] {
   threshold = Math.min(1, Math.max(0, threshold)) / 2;
-  const element_list = [] as boolean [];
-  const element_bounds = element.getBoundingClientRect();
-  const {children, clientLeft, clientTop, scrollLeft, scrollTop} = element;
+
+  const element_list = [] as boolean[];
+  const {children, offsetLeft, offsetTop, clientLeft, clientTop, scrollLeft, scrollTop} = element;
   for (let i = 0; i < children.length; i++) {
     const child = children.item(i);
     if (!child) continue;
 
     const {left, top, width, height} = child.getBoundingClientRect();
-    const x = (left - element_bounds.left - clientLeft + scrollLeft) + threshold * width;
-    const y = top - element_bounds.top - clientTop + scrollTop + threshold * height;
-    const child_rect = new Rect(x, y, width - threshold * width * 2, height - threshold * height * 2);
-    element_list.push(rect.intersectsRect(child_rect));
+    const x = left - offsetLeft - clientLeft + scrollLeft + threshold * width;
+    const y = top - offsetTop - clientTop + scrollTop + threshold * height;
+    element_list.push(rect.intersectsRect(new Rect(x, y, width - threshold * width * 2, height - threshold * height * 2)));
   }
 
   return element_list;
 }
 
 function getEmptyList(element: HTMLElement) {
-  const element_list = [] as boolean [];
-  for (let i = 0; i < element.children.length; i++) {
-    const child = element.children.item(i);
-    if (child) element_list.push(false);
-  }
-  return element_list;
+  return Array(element.children.length).fill(false);
 }
 
 interface Reference {
+  flag_ctrl: boolean;
+  flag_shift: boolean;
   selection_point?: Point;
   selection_rect?: Rect;
   scroll_timer?: number;
@@ -198,16 +287,19 @@ export interface ElementPickerRectCSSProperties {
 }
 
 export interface ElementPickerProps extends React.PropsWithChildren {
-  threshold?: number;
+  click_threshold?: number;
+  select_threshold?: number;
   scroll_coefficient?: number;
 
+  focus?: number;
+  selection?: boolean[]
   style?: {
     border?: Property.Border
     background?: Property.Background
   };
 
   onClick?(values: number | undefined, rect: Rect, event: MouseEvent): void;
-  onHover?(values: boolean[], rect: Rect): void;
+  onHover?(values?: boolean[], rect?: Rect): void;
   onSelect?(values: boolean[], rect: Rect, event: MouseEvent): void;
 }
 
