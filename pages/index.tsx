210aafb705749017ad7dee28efa3b70b1a5deca8
oscelest <oscelest@gmail.com>
2022-08-01 22:38:16 +0200

    Master (#1)
    * Initial commit
    
    * Initial commit
    
    * Test
    
    * Initial commit
    
    Co-authored-by: OSKC <oskc@nnit.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,45 @@
 import type {NextPage} from "next";
-import ElementPicker from "../src/ElementPicker";
 import Style from "./index.module.scss";
-import Rect from "../src/Rect";
 import {useState} from "react";
+import {Rect} from "@noxy/geometry";
+import {ElementPicker} from "../src";
 
 const IndexPage: NextPage = () => {
 
   const [element_list, setElementList] = useState<Array<undefined>>(Array(5).fill(undefined));
   const [hovered, setHovered] = useState<boolean[]>([]);
   const [selected, setSelected] = useState<boolean[]>([]);
-  const [focused, setFocused] = useState<number | undefined>(undefined);
+  const [focused, setFocused] = useState<number[]>();
 
-  function onSelect(values: boolean[], rect: Rect, event: MouseEvent) {
+  function onCommit(values: boolean[], rect?: Rect) {
     setSelected(values);
   }
 
-  function onHover(values: boolean[], rect: Rect, event: MouseEvent) {
+  function onHover(values: boolean[], rect?: Rect) {
     setHovered(values);
+    // console.log("Hover", values);
   }
 
-  function onClick(value: number | undefined, rect: Rect, event: MouseEvent) {
+  function onClick(value?: number[], rect?: Rect) {
     setFocused(value);
   }
 
   return (
-    <ElementPicker onSelect={onSelect} onHover={onHover} onClick={onClick}>
+    <ElementPicker className={Style.ElementPicker} selection={selected} onCommit={onCommit} onHover={onHover} onClick={onClick}>
       {element_list.map((value, index) => {
         const className = [Style.Large];
-        if (focused === index) className.push(Style.Focused);
-        if (selected[index]) {
-          className.push(Style.Selected);
+        if (selected[index] && !hovered[index]) {
+          className.push(Style.Deselected);
         }
-        else if (hovered[index]) {
-          className.push(Style.Hovered);
+        else {
+          if (selected[index]) {
+            className.push(Style.Selected);
+          }
+          if (hovered[index]) {
+            className.push(Style.Hovered);
+          }
         }
+
         return (
           <div className={className.join(" ")} key={index}>{index}</div>
         );
