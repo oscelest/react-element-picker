7f79b808b9dc5c96b2d1f5043c4f9d9bc4d762d8
oscelest <oscelest@gmail.com>
2022-08-06 11:45:24 +0200

    Proper rollup setup.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-}
+/** @type {import("next").NextConfig} */
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: false
+};
+
+module.exports = nextConfig;
